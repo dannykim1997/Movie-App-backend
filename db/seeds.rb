72ab2f348f0582c77506a1082f01f314673db766
github username <github email address>
2021-05-07 15:10:49 -0700

    test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require_relative "../.env"
+
+User.create(username: "username", password: "password")
+Movie.create(title: "Sailor Moon", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8pStsess2hyOe_vy5jIiv1_xMBKqMun0vag&usqp=CAU" , overview: "A schoolgirl and her friends transform into powerful heroes to battle the forces of evil.")
+Review.create(comment: "My favorite hero!", user_id: User.all.sample.id, movie_id: Movie.all.sample.id)
