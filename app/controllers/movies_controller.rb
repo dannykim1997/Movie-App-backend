22fc1ffb43f0475a779644ef31cb7c6c7739cdbb
github username <github email address>
2021-05-10 11:35:59 -0700

    got api

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,14 @@
 class MoviesController < ApplicationController
-    def index
-        @movies = Movie.all
-        render json: @movies
+    require "rest-client"
+    def get_tv_popular
+        url = "https://api.themoviedb.org/3/tv/popular?api_key=a5284168423536e80459a648766c3be1&language-en-US"
+        response = RestClient.get(url)
+        render json: response
+    end
+
+    def trending_movies
+        url = "https://api.themoviedb.org/3/trending/all/week?api_key=a5284168423536e80459a648766c3be1&language-en-US"
+        response = RestClient.get(url)
+        render json: response
     end
 end
