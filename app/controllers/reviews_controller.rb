13f212759400d6bedddb15ed56635b77fe12f9b0
Tri <triphan921@gmail.com>
2021-05-10 13:26:56 -0700

    reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,40 @@
 class ReviewsController < ApplicationController
+
+    before_action :find_review, only: [:show, :edit, :update, :delete]
+
     def index
         @reviews = Review.all
         render json: @reviews
       end
+
+      def show
+      end
+      
+      def new
+        @review = Review.new
+      end
+
+      def create
+        @review = Review.new(params[:review])
+        if @review.save
+          flash[:success] = "Review successfully created"
+          redirect_to @review
+        else
+          flash[:error] = "Something went wrong"
+          redirect_to 'new'
+        end
+      end
+
+      private
+
+      def find_review
+        @review = Review.find_by(id: 
+        params[:id]
+        )
+      end
+
+      def review_params
+        params.require(:review).permit(:comment, :user_id, :movie_id)
+      end
+      
 end
