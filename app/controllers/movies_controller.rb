adbb27bff89d66f1affa639ff10c25751daf504c
github username <github email address>
2021-05-11 11:00:48 -0700

    got seeded api data

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,4 @@
 class MoviesController < ApplicationController
-    # require "rest-client"
-
-    # def trending_movies
-    #     url = "https://api.themoviedb.org/3/movie/popular?api_key=a5284168423536e80459a648766c3be1&language-en-US"
-    #     response = RestClient.get(url)
-    #     render json: response
-    # end
-
     def index
         movies = Movie.all
         render json: MovieSerializer.new(movies).serialized_json
